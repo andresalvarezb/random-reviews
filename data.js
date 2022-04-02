c9507ce0caedab599cd068af1e512d50f77a5c0b
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-04-02 08:55:22 -0500

    feat: add data

---
+++ data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        "quote": "You can avoid reality, but you cannot avoid the consequences of avoiding reality.",
+        "author": "Ayn Rand",
+        "category": "Actress",
+        "image": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "I can write better than anybody who can write faster, and I can write faster than anybody who can write better.",
+        "author": "A. J. Liebling",
+        "category": "Famous",
+        "image": "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "This book fills a much-needed gap.",
+        "author": "Moses Hadas in a review",
+        "category": "Famous",
+        "image": "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "A mathematician is a device for turning coffee into theorems.",
+        "author": "Paula Erdos",
+        "category": "actress",
+        "image": "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "The only difference between me and a madman is that I'm not mad.",
+        "author": "Elizabeth Dali",
+        "category": "artist",
+        "image": "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "Never interrupt your enemy when he is making a mistake.",
+        "author": "Alejandro Bonaparte",
+        "category": "Famous",
+        "image": "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "If you are going through hell, keep going.",
+        "author": "Amy Winston ",
+        "category": "singer",
+        "image": "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "He who has a 'why' to live, can bear with almost any 'how'.",
+        "author": "taylor smith",
+        "category": "writer",
+        "image": "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "I'm all in favor of keeping dangerous weapons out of the hands of fools. Let's start with typewriters.",
+        "author": "Frank Lloyd Wright",
+        "category": "Famous",
+        "image": "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        "quote": "I am ready to meet my Maker. Whether my Maker is prepared for the great ordeal of meeting me is another matter.",
+        "author": "Sir Winston Churchill",
+        "category": "Famous",
+        "image": "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    }
+]
+
+export default data;
